46b21decfe2c444d73f14c7c04cab5be98e60c75
Shubhankar Potnis <shubhankarpotnisofficial@gmail.com>
2025-07-22 19:42:53 +0530

    Added User Classes

---
+++ src/main/java/net/shubhankarpotnis/diaryApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package net.shubhankarpotnis.diaryApp.repository;
+
+
+import net.shubhankarpotnis.diaryApp.entity.DiaryEntry;
+import net.shubhankarpotnis.diaryApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String username);
+}
---
+++ src/main/java/net/shubhankarpotnis/diaryApp/service/UserService.java
@@ -0,0 +1,43 @@
+package net.shubhankarpotnis.diaryApp.service;
+
+import net.shubhankarpotnis.diaryApp.entity.DiaryEntry;
+import net.shubhankarpotnis.diaryApp.entity.User;
+import net.shubhankarpotnis.diaryApp.repository.DiaryEntryRepository;
+import net.shubhankarpotnis.diaryApp.repository.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.logging.ErrorManager;
+
+@Component
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveEntry(User user){
+            userRepository.save(user);
+    }
+
+    public List<User> getAll(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findById(ObjectId id){
+        return userRepository.findById(id);
+    }
+
+    public void deleteById(ObjectId id){
+        userRepository.deleteById(id);
+    }
+
+    public User findByUserName(String username){
+        return userRepository.findByUserName(username);
+    }
+}
+
+// controller ---> service ---> repository
